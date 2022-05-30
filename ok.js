4db8760b71d16c167ab4b8de19955f6c837c0c89
ayush122 <ayush230497@gmail.com>
2022-05-30 22:35:05 +0530

    git commit

---
+++ ok.js
@@ -0,0 +1,8 @@
+// you know its messed up 
+console.log(" Hello");
+function Hello(){
+    console.log(" this a function");
+    console.log(" this  function just gives op");
+
+}
+console.log(Hello());
